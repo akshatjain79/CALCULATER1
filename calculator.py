f430a15f9ec9b917101259ef4f4cc16091ec3ac5
Akshat Jain <akshatjain79875@gmail.com>
2026-09-13 16:06:16 +0530

    V1.0

--- calculator.py
+++ calculator.py
@@ -1,67 +1,43 @@
-#calculator 
-# my first project
-# 3 step to build calculator 
-#1 function for operation
-# user input
-#3 pri
+# python program to creat calculaor
+#ther are 3 basic step to creat puthon file
+#function for operat
+# use input
+#print resule
+# function to add two function to add two numbers
+def add(num1 ,num2):
+    return num1+num2
+#function to substract two number
+def sub(num1 ,num2):
+    return num1-num2
+#function two multiply two number
+def multiply(num1 ,num2):
+    return num1*num2
+#function two divide two number
+def divide(num1 ,num2):
+    return num1/num2
+#function two take average of two number
+def avg(num1 ,num2):
+    return (num1+num2)/2
 
-#function to addtwo number
-def add(num1,num2):
-    return num1 + num2
+#step 2 to take user input
+print("please select your operation")
+print(" 1.additon")
+print("2.substraction")
+print("3.multiply")
+print("4.divide")
+print("5.average")
 
-#unction to multiplytwo number
-def multiply(num1,num2):
-    return num1 * num2
 
-# function to divide two number
-def divide(num1,num2):
-    return num1 / num2
+select=int(input("select an operation from 1,2,3,4,5:"))
+num1=int(input("enter your first number"))
+num2=int(input("enter your secand number"))
 
-# function to substract two number
-def substract(num1,num2):
-    return num1 - num2
 
-# function to take average of two number
-def average(num1,num2):
-    return (num1 + num2) / 2
-# step2; user input
-print("please select an operator to perform the calculation:")
-print("1. Add")
-print("2. Multiply")
-print("3. Divide")
-print("4. Substract")
-print("5. Average")
-
-select =int(input("select a operation form 1,2,3,4,5:"))
-num1 =int(input("enter first number:"))
-num2 =int(input("enter second number:"))
-
-# step 3 ; print the result
-if (select == 1):
-    print(num1, "+" , num2, "=",\
+# her the final move== RESULT
+if(select==1):
+    print(num1,"+",num2,"="/
           add(num1,num2))
-
-elif (select == 2):
-    print(num1, "*" , num2, "=",\
-          multiply(num1,num2))
-
-elif (select == 3):
-    print(num1, "/" , num2, "=",\
-          divide(num1,num2))
-
-elif (select == 4):
-    print(num1, "-" , num2, "=",\
-          substract(num1,num2))
-
-elif (select == 5):
-    print(num1, "+" , num2,")", "=","2","=",\
-          substract(num1,num2))
+    else if(select)
 
 
-else:
-    print("invalid operation! pls select again")
-   
-    
-    
-    
-    
+
